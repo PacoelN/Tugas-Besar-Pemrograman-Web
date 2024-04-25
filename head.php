27473f5558ea82a7074d11d5e19abc4d299f1793
Ftthreign <theownerkill432@gmail.com>
2024-04-25 22:54:46 +0700

    chore: add php initializer

---
+++ head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Tugas Besar Pemrograman Web" ?></title>
+    <link rel="stylesheet" href="./style/bootstrap.min.css" />
+    <link rel="stylesheet" href="/style/index.css" />
+    <link rel="shortcut icon" href="./dist/assets/brand.svg" type="image/x-icon" />
+</head>
+
+<body>
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+</body>
+
+</html>
